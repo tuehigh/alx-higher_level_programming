90502fc02c2eaec2f08c34509af84f6cc2c9f434
tuehigh <c0772323281@gmail.com>
2023-11-03 16:15:07 -0700

    add

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add_0 as add
-    a = 1
-    b = 2
+    import calculator_1 as calc
+    a = 10
+    b = 5
 
-    result = add.add(a, b)
+    result = calc.add(a, b)
     print("{:d} + {:d} = {:d}".format(a, b, result))
