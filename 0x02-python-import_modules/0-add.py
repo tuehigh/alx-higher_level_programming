7a57cbc14c5176077c7f0c178683b016dab25d0a
tuehigh <c0772323281@gmail.com>
2023-11-03 16:19:49 -0700

    push

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add_0 as add
+    from add_0 import add
     a = 1
     b = 2
 
-    result = add.add(a, b)
+    result = add(a, b)
     print("{:d} + {:d} = {:d}".format(a, b, result))
