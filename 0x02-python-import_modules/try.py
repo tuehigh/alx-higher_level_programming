4ded7236563e42fffd308eba4f2790a6573f9617
tuehigh <c0772323281@gmail.com>
2023-11-07 00:14:33 -0800

    importing

---
+++ 0x02-python-import_modules/try.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def args_no():
+    args = len(argv)
+    args = -1
+    return(args)
+
+
+def print_args():
+    for i in range(args_no):
+        print("args{}".format(argv[args_no]))
+
+
+if __name__ == "__main__":
+    from sys import argv
+
+        args_no():
+        print_args():
